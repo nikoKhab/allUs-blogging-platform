021eb1a2d282c60a53f4fd5358420640b187eeb8
nikoKhab <nmangori.1@gmail.com>
2025-01-25 20:16:56 +0400

    email and password authentication done

--- app/src/app/firebase/init.js
+++ app/src/app/firebase/init.js
@@ -1,10 +1,26 @@
-import { initializeApp } from 'firebase/app';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: Replace the following with your app's Firebase project configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  //...
+  apiKey: "AIzaSyCSUGJ-jwpxYXEvn6ErlUjTPNJbDDe2TXY",
+  authDomain: "allusdb.firebaseapp.com",
+  projectId: "allusdb",
+  storageBucket: "allusdb.firebasestorage.app",
+  messagingSenderId: "297667933701",
+  appId: "1:297667933701:web:62a2bda6f7652bab3e4e6c",
+  measurementId: "G-7FPC3T0ZWK"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const auth = getAuth(app);
 
-export default app
+
+export { app, auth };
--- app/src/app/signup/page.tsx
+++ app/src/app/signup/page.tsx
@@ -1,21 +1,72 @@
+"use client"
+import { useRef, useState } from "react";
 import Styles from "./page.module.css";
 import Button from "../components/googleButton/Button";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/app/firebase/init";
+
 const Page = () => {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  
+  const [userSignUpForm, setUserSignUpForm] = useState({
+    userEmail: "",
+    userPassword: ""
+  });
+
+  const createUser = () => {
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+
+    if (email && password) {
+      setUserSignUpForm({
+        userEmail: email,
+        userPassword: password,
+      });
+
+      //firebase function
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log("it did work bro");
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode, errorMessage, error);
+          console.log("BRUH");
+        });
+    } else {
+      console.log("Please fill out both fields");
+    }
+  };
+
   return (
     <>
       <h1 className={Styles.title}>Sign up</h1>
 
       <center>
         <div className={Styles.mainCont}>
-
           <form className={Styles.form}>
-            <input type="text" name="email" placeholder="enter your email" className={Styles.input}/>
-            <input type="text" name="password" placeholder="enter your password" className={Styles.input}/>
-            <button type="submit">submit</button>
+            <input
+              type="text"
+              name="email"
+              placeholder="enter your email"
+              className={Styles.input}
+              ref={emailRef}
+            />
+            <input
+              type="text"
+              name="password"
+              placeholder="enter your password"
+              className={Styles.input}
+              ref={passwordRef}
+            />
           </form>
+          <button onClick={createUser}>submit</button>
 
-          <Button></Button>
-
+          <Button />
         </div>
       </center>
     </>
